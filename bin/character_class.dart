7152db66901a07aeb26510745b73d7158a5aa693
jinju9513 <wjswlswn99@naver.com>
2025-03-21 10:51:56 +0900

    refactor : 파일 읽어오기 예외처리

--- bin/character_class.dart
+++ bin/character_class.dart
@@ -14,8 +14,18 @@ class Character {
 
   Future<void> loadCharacter() async {
     //캐릭터 데이터 파일 읽어오는 메서드
+
     var character = File('lib/characters.txt'); //characters파일 읽어오기
+
+    if (!character.existsSync()) {
+      throw Exception("⚠ 오류: characters.txt 파일을 찾을 수 없습니다.");
+    }
     List<String> characterList = await character.readAsLines();
+
+    if (characterList.isEmpty) {
+      throw Exception("⚠ 오류: characters.txt 파일이 비어 있습니다.");
+    }
+
     for (var charValue in characterList) {
       List<String> charValues = charValue.split(',');
 
@@ -24,11 +34,13 @@ class Character {
       chPower = int.parse(charValues[1]);
       chDefense = int.parse(charValues[2]);
     }
+    applyBonusHealth();
   }
 
   //30% 확률로 체력 증가 기능
-   void applyBonusHealth() {
-    if (random.nextDouble() < 0.3) { // 0.0 ~ 0.99 사이 난수 발생 (30% 확률)
+  void applyBonusHealth() {
+    if (random.nextDouble() < 0.3) {
+      // 0.0 ~ 0.99 사이 난수 발생 (30% 확률)
       chHealth += 10;
       print('🎉 보너스 체력을 얻었습니다! 현재 체력: $chHealth');
     }
@@ -41,14 +53,15 @@ class Character {
 
   //공격 메서드
   void attackMonster(Monster monster) {
-    int damage = max(chPower, 0);//공격력은 최소 0이상
-    monster.monHealth -=damage; //몬스터 체력감소
+    int damage = max(chPower, 0); //공격력은 최소 0이상
+    monster.monHealth -= damage; //몬스터 체력감소
     print('$chName이(가) ${monster.monName}에게 $damage의 데지미를 입혔습니다.');
   }
+
   //방어 메서드
   void defend() {
     int healAmount = 0; //방어시 회복량
-    chHealth +=healAmount;
+    chHealth += healAmount;
     print('$chName이(가) 방어태세를 취하여 $healAmount 만큼 체력을 얻었습니다.');
   }
 }
